2a8a82632a4aad154d968bb39242b072ae641219
JamesP1996 <jay@Jay-PC.localdomain>
2021-02-07 23:39:16 +0000

    Reviewing Bit Operations

--- bit-operations.c
+++ bit-operations.c
@@ -1,31 +1,44 @@
 #include <stdio.h>
 
-void bin_print(int i)
+void bin_print(unsigned int i)
 {
-    int j = sizeof(int) * 8;
+    // Number of bits in an integer.
+    int j = sizeof(unsigned int) * 8;
 
     // Temporary Variable 
     int k;
-
+    
+    // Loop over the Bits in I
     for(j--;j >= 0; j--){
+	 //  
    	 k = ((1 << j) & i) ? 1 : 0;
+	 // Print K
 	 printf("%d",k);
     }
 }
+
+// Standard Main Signature
 int main(int argc, char *argv[])
 {
-    int i = 0xf1;
+    // Set I to a Literal Value;
+    unsigned int i = 0xffffffff;
     
+    // What we're printing.
+    printf("Original: ");
+    // Print i in binary
     bin_print(i);
-    printf("\n");
+    // End Line.
+    printf("\t%x\t%u\n",i,i);
     
     for(int j = 0; j < 40; j++)
     {
-    printf("%3d << %2d " , i, j);
-    bin_print(i<<j);
+    // What the operation is.
+    printf("%3u << %2d " , i, j);
+    // i Shifted Left << j times
+    bin_print(i << j);
     printf("\n");
 
     }
-
+    // End the Main Function
     return 0;
 }
